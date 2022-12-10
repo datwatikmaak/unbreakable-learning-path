da247f13eae098f6b12b0165d34acb4ff997283b
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-12-10 11:27:49 +0100

    completed function dedup_and_title_case_names(names)

--- 5/names.py
+++ 5/names.py
@@ -1,13 +1,27 @@
-NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-         'julian sequeira', 'sandra bullock', 'keanu reeves',
-         'julbob pybites', 'bob belderbos', 'julian sequeira',
-         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+NAMES = [
+    "arnold schwarzenegger",
+    "alec baldwin",
+    "bob belderbos",
+    "julian sequeira",
+    "sandra bullock",
+    "keanu reeves",
+    "julbob pybites",
+    "bob belderbos",
+    "julian sequeira",
+    "al pacino",
+    "brad pitt",
+    "matt damon",
+    "brad pitt",
+]
 
 
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
-       each name appears only once"""
-    pass
+    each name appears only once"""
+    return list({name.title() for name in NAMES})
+
+
+dedup_and_title_case_names(NAMES)
 
 
 def sort_by_surname_desc(names):
@@ -18,7 +32,7 @@ def sort_by_surname_desc(names):
 
 def shortest_first_name(names):
     """Returns the shortest first name (str).
-       You can assume there is only one shortest name.
+    You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
     # ...
