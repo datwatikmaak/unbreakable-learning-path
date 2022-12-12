1a599c270d23c80a6ab053ed10b6c5725bb12d1c
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-12-12 06:57:27 +0100

    submission Bite 16 @ codechalleng.es

--- 16/gendates.py
+++ 16/gendates.py
@@ -1,7 +1,15 @@
 from datetime import datetime, timedelta
+from pprint import pprint
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
+pprint(PYBITES_BORN)
 
 
 def gen_special_pybites_dates():
-    pass
+    from_date = PYBITES_BORN + timedelta(days=100)
+    while True:
+        yield from_date
+        from_date = from_date + timedelta(days=100)
+
+
+gen_special_pybites_dates()
