e608785a89db8474246bdb28c6e99a69ce6a0242
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2022-12-25 08:26:36 +0100

    submission Bite 44 @ codechalleng.es

--- 44/license.py
+++ 44/license.py
@@ -1,4 +1,7 @@
-# use one or more Standard Library modules
+import secrets
+import string
+
+alphabet = string.ascii_uppercase + string.digits
 
 
 def gen_key(parts: int = 4, chars_per_part: int = 8) -> str:
@@ -12,4 +15,5 @@ def gen_key(parts: int = 4, chars_per_part: int = 8) -> str:
     If parts = 3 and chars_per_part = 4 a random license
     key would look like this: 54N8-I70K-2JZ7
     """
-    pass
+    chars = [secrets.choice(alphabet) for _ in range(chars_per_part * parts)]
+    return "-".join(("".join(i) for i in [chars[i::parts] for i in range(parts)]))
