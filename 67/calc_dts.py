346ccd55f3cdce2204ec75ef3d883f485e044ac6
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2023-01-05 01:08:52 +0100

    submission Bite 67 @ codechalleng.es

--- 67/calc_dts.py
+++ 67/calc_dts.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 start_100days = date(2017, 3, 30)
 pybites_founded = date(2016, 12, 19)
@@ -7,9 +7,17 @@ pycon_date = date(2018, 5, 8)
 
 def get_hundred_days_end_date():
     """Return a string of yyyy-mm-dd"""
-    pass
+    end_date = start_100days + timedelta(days=100)
+    return datetime.strftime(end_date, "%Y-%m-%d")
+
+
+get_hundred_days_end_date()
 
 
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
-    pass
+    delta = pycon_date - pybites_founded
+    return delta.days
+
+
+get_days_between_pb_start_first_joint_pycon()
